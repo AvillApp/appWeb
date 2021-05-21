5b060fb02988f882ff547133b8dbc30da7d4b383
INGJEISSON <jeisim18@gmail.com>
2021-05-21 11:41:50 -0500

    Update index.js

--- app/api/db/index.js
+++ app/api/db/index.js
@@ -1,4 +1,4 @@
-export const api = "https://intranet.avill.com.co/api/";
+export const api = "https://api.avill.com.co/api/";
 //export const api = "http://192.168.1.51/Webapp/app/controller/";
 
 //export const api = "http://localhost:8000/api/";
